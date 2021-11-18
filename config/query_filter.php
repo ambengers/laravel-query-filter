3dde1a511417469849567f48211915b72bd3c1e1
Marvin Quezon <quezon.marvin@gmail.com>
2021-11-18 10:39:47 +0800

    Updates comments.

--- config/query_filter.php
+++ config/query_filter.php
@@ -2,12 +2,12 @@
 
 return [
 
-    // The method to call to use the query filter
+    // The method to call on your model to use the query filter.
     'method' => 'filter',
 
-    // The namespace to use for query filter classes
+    // The namespace to use for query filter classes.
     'filter_namespace' => 'App\Filters',
 
-    // The namespace to use for query loader classes
+    // The namespace to use for query loader classes.
     'loader_namespace' => 'App\Loaders',
 ];
